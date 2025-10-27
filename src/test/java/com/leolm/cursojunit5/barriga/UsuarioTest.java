0d4179af98d85930a2ae65c1f78e130516584ff6
leolm83 <leonardo.mateus01@hotmail.com>
2025-10-26 23:35:03 -0300

    feat: importacao dados de teste do csv

--- src/test/java/com/leolm/cursojunit5/barriga/UsuarioTest.java
+++ src/test/java/com/leolm/cursojunit5/barriga/UsuarioTest.java
@@ -10,6 +10,9 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
+import org.junit.jupiter.params.provider.CsvSource;
 
 import com.leolm.cursojunit5.barriga.builders.UsuarioBuilder;
 import com.leolm.cursojunit5.barriga.validations.ViolationHandler;
@@ -95,6 +98,57 @@ public class UsuarioTest {
                 .umUsuario()
                 .valido()
                 .withSenha("")
+                .build();
+
+        Set<ConstraintViolation<Usuario>> violations = validator.validate(usuario);
+        ViolationHandler.showViolations(violations);
+        boolean campoEstaInvalido = ViolationHandler.fieldHasViolations(nomeCampoEmTeste, violations);
+        Assertions.assertTrue(!violations.isEmpty() && campoEstaInvalido);
+    }
+
+    // para nomear um campo no teste basta passar esse parametro name
+    @ParameterizedTest(name = "[{index}] - valida campo : {3}")
+    @CsvSource(value = {
+            "NULL,'leoncio',123456789012,id", // sem id
+            "2,NULL,123456789012,nome", // sem nome
+            "3,'leoncio',NULL,senha", // sem senha
+            "3,'leoncio',,senha" // sem senha porem utilizando o default null value que é vazio
+    /*
+     * Please note that unquoted empty values will always be converted to null
+     * references regardless of the value of this nullValues attribute; whereas, a
+     * quoted empty string will be treated as an emptyValue().
+     * src:
+     * https://stackoverflow.com/questions/65207427/junit-5-how-to-pass-in-multiple-
+     * null-values-for-csvsource
+     */
+    }, nullValues = { "NULL" })
+    public void deveRejeitarUsuarioSemSenha(Long id, String nome, String senha, String nomeCampoEmTeste) {
+        Usuario usuario = UsuarioBuilder
+                .umUsuario()
+                .withId(id)
+                .withName(nome)
+                .withSenha(senha)
+                .build();
+
+        Set<ConstraintViolation<Usuario>> violations = validator.validate(usuario);
+        ViolationHandler.showViolations(violations);
+        boolean campoEstaInvalido = ViolationHandler.fieldHasViolations(nomeCampoEmTeste, violations);
+        Assertions.assertTrue(!violations.isEmpty() && campoEstaInvalido);
+    }
+
+    // para nomear um campo no teste basta passar esse parametro name
+    @ParameterizedTest(name = "[{index}] - valida campo : {3}")
+    @CsvFileSource(files = { "src/test/resources/CamposObrigatoriosUsuario.csv" }, nullValues = {
+            "NULL" }, useHeadersInDisplayName = true
+    // numLinesToSkip = 1 // utilizado para pular linhas do csv
+    )
+    public void deveRejeitarUsuarioSemSenhaComValoresImportadosDoCSV(Long id, String nome, String senha,
+            String nomeCampoEmTeste) {
+        Usuario usuario = UsuarioBuilder
+                .umUsuario()
+                .withId(id)
+                .withName(nome)
+                .withSenha(senha)
                 .build();
 
         Set<ConstraintViolation<Usuario>> violations = validator.validate(usuario);
